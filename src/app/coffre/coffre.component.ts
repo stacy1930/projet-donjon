c0f1324486e3f34f7c346e61e06364c18b0b7690
stacy perales <stacy.perales@ynov.com>
2022-04-28 21:39:12 +0200

    affichage headers dans coffre

--- src/app/coffre/coffre.component.ts
+++ src/app/coffre/coffre.component.ts
@@ -11,7 +11,14 @@ export class CoffreComponent implements OnInit {
   public data: any;
   public coffre: any;
   public ciffer: any;
+  public testStacy: any;
+  public contentHeaders: any;
+  public array1: string[] = [];
+  public array2: string[] = [];
+  // var arr = { "un" : 1, "deux" : 2, "trois": 3 };
+
   constructor(private apiService: ApiService) { }
+
 
   ngOnInit(): void {
 
@@ -19,8 +26,26 @@ export class CoffreComponent implements OnInit {
       this.coffre = this.apiService.getCoffre(localStorage.getItem('Authorization')).subscribe(data => {
         console.log(data.headers.get('cipher'));
         this.data = data;
-        
+
         this.ciffer = data.headers.get('cipher');
+
+        this.contentHeaders = data.headers;
+        //@ts-ignore
+        this.testStacy = data.headers.headers;
+        console.log(this.contentHeaders);
+
+        this.testStacy.forEach((element: any) => {
+          console.warn(element);
+          this.array1.push(element);
+        });
+        console.warn(this.array1);
+
+        this.contentHeaders.forEach((element: any) => {
+          console.warn(element);
+          this.array2.push(element);
+
+        });
+
       }
       );
     }
